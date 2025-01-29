ed1a137ab1296d2c698b9436e394b4c53b47d1c1
Chris Hartgerink <chris@libscie.org>
2025-01-29 10:15:47 +0100

    Update `drop_safeframe()`

--- tests/testthat/test-drop_safeframe.R
+++ tests/testthat/test-drop_safeframe.R
@@ -1,8 +1,11 @@
 test_that("tests for drop_safeframe", {
-  x <- make_safeframe(cars, speed = "Miles per hour")
+  x <- make_safeframe(cars, mph = "speed")
   expect_identical(cars, drop_safeframe(x, remove_tags = TRUE))
 
   y <- drop_safeframe(x, remove_tags = FALSE)
-  expect_identical(tags(x, TRUE)$speed, attr(y$speed, "label"))
-  expect_identical(tags(x, TRUE)$dist, attr(y$dist, "label"))
+  expect_identical(tags(x), tags(y))
+  
+  y <- drop_safeframe(x, remove_tags = TRUE)
+  expect_null(attr(y$speed, "label"))
+  expect_null(attr(y$dist, "label"))
 })
