b29dab58405604a97a79b1124c02ca841d2867b0
Chris Hartgerink <chris@libscie.org>
2025-02-12 13:00:19 +0100

    Update class retention in `[.safeframe` and `[<-.safeframe`

--- R/square_bracket.R
+++ R/square_bracket.R
@@ -92,8 +92,10 @@
 
   # Case 2
   old_tags <- tags(x)
+  old_classes <- class(x)
   out <- restore_tags(out, old_tags, lost_action)
-
+  class(out) <- old_classes
+  
   out
 }
 
@@ -105,7 +107,10 @@
   lost_action <- get_lost_tags_action()
   out <- NextMethod()
   old_tags <- tags(x)
+  old_classes <- class(x)
+  
   out <- restore_tags(out, old_tags, lost_action)
+  class(out) <- old_classes
 
   out
 }
@@ -122,6 +127,7 @@
   lost_tags(old_tags, new_tags, lost_action)
 
   class(x) <- setdiff(class(x), "safeframe")
+  
   x <- NextMethod()
 
   # Call restore_tags to restore the tags
