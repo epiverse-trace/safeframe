df5aabd1384f490eafe5d1bebf91f56356c70042
Chris Hartgerink <chris@libscie.org>
2025-02-20 11:59:47 +0530

    Implement class inheritance checking (#70)
    * Add reprex as provided by @TimTaylor
    
    * Add custom `check_multi_class`
    
    * Update lint file
    
    * Upgrade min R >= 4.0.0; fix lint issues

--- R/validate_types.R
+++ R/validate_types.R
@@ -47,7 +47,7 @@ validate_types <- function(x, ...) {
     vars_to_check,
     function(var) {
       allowed_types <- types[[var]]
-      checkmate::check_multi_class(
+      check_multi_class(
         x[[tags(x)[[var]]]],
         allowed_types,
         null.ok = TRUE
@@ -69,4 +69,36 @@ validate_types <- function(x, ...) {
   }
 
   x
+}
+
+#' Internal function
+#' Custom implementation of checkmate's check_multi_class to ensure
+#' proper handling of multi-class situations as reported in #44.
+#' @param x Object to check classes for
+#' @param classes Character vector of class names to check against
+#' @param null.ok Logical indicating whether NULL is allowed (default: FALSE)
+#' @noRd
+check_multi_class <- function(x, classes, null.ok = FALSE) {
+  checkmate::qassert(classes, "S+")
+  checkmate::qassert(null.ok, "B1")
+  if (is.null(x) && null.ok) {
+    return(TRUE)
+  }
+
+  # Get all classes including inherited ones
+  obj_classes <- .class2(x)
+
+  if (!any(classes %in% obj_classes)) {
+    cl <- class(x)
+    return(sprintf(
+      "Must inherit from class '%s', but has class%s '%s'",
+      paste(classes, collapse = "'/'"), if (length(cl) >
+        1L) {
+        "es"
+      } else {
+        ""
+      }, paste(cl, collapse = "','")
+    ))
+  }
+  return(TRUE)
 }
